c7ce4c58ca3be0a6c8b318b112dd9872997cded5
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-04-12 16:51:32 +0300

    app fonts added

---
+++ lib/presentation/assets/app_fonts.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppFonts {
+  TextStyle regular12 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+  TextStyle regular12_108_5 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.085,
+  );
+  TextStyle regular12_135 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.35,
+  );
+  TextStyle regular12_144_5 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.445,
+  );
+  TextStyle regular13 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+  );
+  TextStyle regular14 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  TextStyle regular14_121 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.21,
+  );
+  TextStyle regular14_132_5 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.325,
+  );
+  TextStyle regular14_135 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.35,
+  );
+  TextStyle regular16 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  TextStyle regular16_135 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.35,
+  );
+  TextStyle regular16_160_5 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.605,
+  );
+  TextStyle regular18 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
+  TextStyle regular20_132_5 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    height: 1.325,
+  );
+  TextStyle medium12 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+  TextStyle medium13 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+  );
+  TextStyle medium14 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+  TextStyle medium14_135 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 1.35,
+  );
+  TextStyle medium16 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+  TextStyle medium24 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  );
+  TextStyle medium28 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+  );
+  TextStyle semiBold13 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+  );
+  TextStyle semiBold16 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+  TextStyle semiBold16_132_5 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    height: 1.325,
+  );
+  TextStyle semiBold18 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  TextStyle semiBold18_130 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    height: 1.3,
+  );
+  TextStyle semiBold24 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    height: 1.3,
+  );
+  TextStyle bold11 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 11,
+    fontWeight: FontWeight.w700,
+  );
+  TextStyle bold20 = GoogleFonts.jost(
+    color: Colors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+  TextStyle yesevaRegular18 = GoogleFonts.yesevaOne(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
+  TextStyle yesevaRegular24 = GoogleFonts.yesevaOne(
+    color: Colors.black,
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+  );
+}
