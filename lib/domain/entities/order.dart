b7392d8ee846268391aec8260f8f36bfd9d194c8
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-04-14 13:00:02 +0300

    order entity updated

--- lib/domain/entities/order.dart
+++ lib/domain/entities/order.dart
@@ -3,7 +3,15 @@ part of 'entities.dart';
 @freezed
 class Order with _$Order {
   factory Order({
+    required int number,
+    required String status,
+    required int productsNumber,
+    required int generalDiscount,
+    required int deliveryPrice,
+    required int generalPrice,
+    required List<Product> productList,
     required Address deliveryAddress,
+    required DateTime orderingDateTime,
     required DateTime deliveryDateTime,
     required PaymentMethod paymentMethod,
     @Default(false) bool writeOffPoints,
