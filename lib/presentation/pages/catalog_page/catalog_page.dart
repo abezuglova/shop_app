5ad43890a5aea1044e79d89f153ac7f8e194f842
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-04-18 11:58:26 +0300

    buttons added to appbars of main and catalog pages

--- lib/presentation/pages/catalog_page/catalog_page.dart
+++ lib/presentation/pages/catalog_page/catalog_page.dart
@@ -15,16 +15,19 @@ class CatalogPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.scaffoldBackgroundColor,
       appBar: CustomAppBar(
-        title: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SvgPicture.asset(AppIcons.smallDeliveryIcon),
-            const SizedBox(width: 8),
-            Text(
-              'ул. Пушкина 15, д. 20, кв. 113',
-              style: AppFonts.medium14,
-            ),
-          ],
+        title: TextButton(
+          onPressed: () {},
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SvgPicture.asset(AppIcons.smallDeliveryIcon),
+              const SizedBox(width: 8),
+              Text(
+                'ул. Пушкина 15, д. 20, кв. 113',
+                style: AppFonts.medium14,
+              ),
+            ],
+          ),
         ),
       ),
       body: CatalogScreen(
