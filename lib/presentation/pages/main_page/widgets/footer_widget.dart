497f45d384f609a852b99ddf9c751c8d754c2d98
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-04-18 15:21:46 +0300

    localizations generated

--- lib/presentation/pages/main_page/widgets/footer_widget.dart
+++ lib/presentation/pages/main_page/widgets/footer_widget.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:third_task/presentation/utils/app_colors.dart';
 import 'package:third_task/presentation/utils/app_fonts.dart';
 
--- lib/presentation/pages/personal_data_page/screens/personal_data_screen.dart
+++ lib/presentation/pages/personal_data_page/screens/personal_data_screen.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:third_task/domain/entities/entities.dart';
 import 'package:third_task/presentation/utils/app_colors.dart';
 import 'package:third_task/presentation/utils/app_fonts.dart';
