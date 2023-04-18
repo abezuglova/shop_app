d31eed260ebdee44850a1000ef99a5952a9b3165
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-04-18 11:49:29 +0300

    clip oval added to advertisement card widget

--- lib/presentation/pages/main_page/widgets/advertisement_card_widget.dart
+++ lib/presentation/pages/main_page/widgets/advertisement_card_widget.dart
@@ -12,81 +12,97 @@ class AdvertisementCardWidget extends StatelessWidget {
     return SizedBox(
       width: 335,
       height: 208,
-      child: Stack(
-        children: [
-          Container(
-            width: 335,
-            height: 208,
-            clipBehavior: Clip.hardEdge,
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: AppColors.promoColor1,
-              borderRadius: const BorderRadius.all(
-                Radius.circular(8),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: AppColors.advertisementShadowColor1.withOpacity(0.5),
-                  offset: const Offset(0, 10),
-                  blurRadius: 20,
-                  spreadRadius: -13,
-                ),
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 233,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        advertisement.name,
-                        style: AppFonts.yesevaRegular24,
-                      ),
-                      const SizedBox(height: 8),
-                      SizedBox(
-                        width: 175,
-                        child: Text(
-                          advertisement.description,
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                          style: AppFonts.regular14_135,
-                        ),
-                      ),
-                    ],
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(8),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              width: 335,
+              height: 208,
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: AppColors.promoColor1,
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.advertisementShadowColor1.withOpacity(0.5),
+                    offset: const Offset(0, 10),
+                    blurRadius: 20,
+                    spreadRadius: -13,
                   ),
-                ),
-                const Spacer(),
-                SizedBox(
-                  width: 131,
-                  height: 35,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Узнать больше',
-                      style: AppFonts.medium14_135.copyWith(
-                        color: Colors.white,
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 233,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          advertisement.name,
+                          style: AppFonts.yesevaRegular24,
+                        ),
+                        const SizedBox(height: 8),
+                        SizedBox(
+                          width: 175,
+                          child: Text(
+                            advertisement.description,
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: AppFonts.regular14_135,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    width: 131,
+                    height: 35,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Узнать больше',
+                        style: AppFonts.medium14_135.copyWith(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
+                ],
+              ),
+            ),
+            Positioned(
+              left: 150,
+              top: 30,
+              child: ClipOval(
+                clipper: _MyClip(),
+                child: Image.network(
+                  advertisement.image,
+                  width: 200,
+                  height: 200,
                 ),
-              ],
+              ),
             ),
-          ),
-          Positioned(
-            left: 200,
-            top: 50,
-            child: Image.network(
-              advertisement.image,
-              width: 200,
-              height: 200,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+}
+
+class _MyClip extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    return const Rect.fromLTWH(52, 40, 200, 200);
+  }
+
+  @override
+  bool shouldReclip(oldClipper) {
+    return false;
   }
 }
