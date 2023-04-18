60260fbfb8578c604aa2dc3f6890d8e9c2686342
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-04-18 15:58:26 +0300

    localizations added to recommendations widget

--- lib/presentation/ui_kit/recommendations_widget.dart
+++ lib/presentation/ui_kit/recommendations_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:third_task/domain/entities/entities.dart';
 import 'package:third_task/presentation/utils/app_fonts.dart';
 import 'package:third_task/presentation/ui_kit/product_card_widget.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class RecommendationsWidget extends StatelessWidget {
   final List<Product> recommendations;
@@ -9,13 +10,14 @@ class RecommendationsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Text(
-            'Рекомендуем',
+            l10n.recommend,
             style: AppFonts.regular20_132_5,
           ),
         ),
